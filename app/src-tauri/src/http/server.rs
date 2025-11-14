5d449eafef18914ed4c59cbafb910b1ce36334cb
Rikelmy Lopes <rikelmylopes899@gmail.com>
2025-11-14 15:52:19 -0300

    substitui o rocket pelo axum

--- app/src-tauri/src/http/server.rs
+++ app/src-tauri/src/http/server.rs
@@ -1,64 +1,100 @@
 use crate::fs::fs::get_dir_entries;
-use crate::state::state::APP_STATE;
+use axum::body::Body;
+use axum::http::{header, HeaderValue, Response, StatusCode};
+use axum::response::{AppendHeaders, IntoResponse};
+/* use crate::state::state::APP_STATE; */
+use axum::routing::get;
+use axum::Router;
 use local_ip_address::local_ip;
-use rocket::fs::{relative, FileServer, NamedFile};
-use rocket::{get, routes, Config};
+use once_cell::sync::Lazy;
 use std::env::home_dir;
-use std::net::IpAddr;
-use std::path::Path;
+use std::sync::Mutex;
+use tokio::fs;
+use tokio::sync::oneshot;
+use tokio_util::io::ReaderStream;
 
-#[get("/entries")]
-fn get_entries() -> String {
+static STOP_TX: Lazy<Mutex<Option<oneshot::Sender<()>>>> = Lazy::new(|| Mutex::new(None));
+
+async fn get_entries() -> impl IntoResponse {
     let files = get_dir_entries();
 
-    return serde_json::to_string(&files).unwrap();
+    let json = serde_json::to_string(&files).unwrap();
+
+    return json;
 }
 
-#[get("/download")]
-async fn download() -> Option<NamedFile> {
-    let home = home_dir().unwrap();
-    let path = Path::new(&home).join("Coding").join("SIGP_INT.jar");
-    NamedFile::open(path).await.ok()
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn download() -> impl IntoResponse {
+    let path = "C:\\Users\\SI30\\Coding\\SIGP_INT.jar";
+
+    let metadata = match fs::metadata(path).await {
+        Ok(m) => m,
+        Err(err) => return Err((StatusCode::NOT_FOUND, format!("Metadata error: {}", err))),
+    };
+
+    let file_size = metadata.len().to_string();
+
+    let file = match tokio::fs::File::open(path).await {
+        Ok(file) => file,
+        Err(err) => return Err((StatusCode::NOT_FOUND, format!("File not found: {}", err))),
+    };
+    let stream = ReaderStream::new(file);
+
+    let body = Body::from_stream(stream);
+
+    let mut response = Response::new(body);
+    let headers = response.headers_mut();
+
+    headers.insert(
+        header::CONTENT_TYPE,
+        HeaderValue::from_static("application/octet-stream"),
+    );
+
+    headers.insert(
+        header::CONTENT_DISPOSITION,
+        HeaderValue::from_static("attachment; filename=\"SIGP_INT.jar\""),
+    );
+
+    headers.insert(
+        header::CONTENT_LENGTH,
+        HeaderValue::from_str(&file_size.to_string()).unwrap(),
+    );
+
+    Ok(response)
 }
 
 #[tauri::command]
 pub async fn start_server(port: u16) -> () {
     let ip = get_current_ip();
+    let (tx, rx) = oneshot::channel::<()>();
 
-    let config = Config {
-        address: Result::expect(ip.parse::<IpAddr>(), "Error converting string to Address!"),
-        port: port,
-        ..Config::debug_default()
-    };
+    *STOP_TX.lock().unwrap() = Some(tx);
 
-    let result = rocket::custom(&config)
-        .mount("/", routes![get_entries, download])
-        .mount("/", FileServer::from(relative!("../web-ui/webapp")))
-        .ignite()
-        .await;
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/download", get(download))
+        .route("/entries", get(get_entries));
 
-    let rocket = match result {
-        Ok(rocket) => rocket,
-        Err(e) => {
-            eprintln!("❌ Erro ao inicializar o Rocket: {}", e);
-            return ();
-        }
-    };
+    let url = format!("{}:{}", ip, port);
 
-    {
-        let mut state = APP_STATE.lock().unwrap();
-        state.shutdown = Some(rocket.shutdown());
-    }
+    println!("Server running on: http://{}", url);
 
-    let _ = rocket.launch().await;
+    let listener = tokio::net::TcpListener::bind(url).await.unwrap();
+
+    let server = axum::serve(listener, app).with_graceful_shutdown(async {
+        rx.await.ok();
+    });
+
+    server.await.unwrap()
 }
 
 #[tauri::command]
 pub fn stop_server() -> () {
-    let state = APP_STATE.lock().unwrap();
-
-    if let Some(shutdown) = state.shutdown.as_ref() {
-        shutdown.clone().notify();
+    if let Some(tx) = STOP_TX.lock().unwrap().take() {
+        let _ = tx.send(());
     }
 }
 
--- app/src-tauri/src/state/state.rs
+++ app/src-tauri/src/state/state.rs
@@ -1,8 +1,7 @@
-use rocket::Shutdown;
 use std::sync::Mutex;
 
-pub struct AppState {
+/* pub struct AppState {
     pub shutdown: Option<Shutdown>,
 }
 
-pub static APP_STATE: Mutex<AppState> = Mutex::new(AppState { shutdown: None });
+pub static APP_STATE: Mutex<AppState> = Mutex::new(AppState { shutdown: None }); */
